4ae827ad08566d415aee989d1e1189b7583def8e
Suraj Kumar <surajkmr20167@gmail.com>
2023-07-18 10:30:56 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -502,6 +502,46 @@ app.post('/api/autofill', async (req, res) => {
 });
 
 
+app.post('/api/autofill2', async (req, res) => {
+
+    // Access the request body data
+    const { operationType, opertaion, updates, productName, policyholderLocator } = req.body;
+
+    const zip_code = updates.updateExposures.fieldValues.ZIP_Code;
+
+    const url = 'https://us-zip-code-information.p.rapidapi.com/?zipcode=' + zip_code;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.api_key,
+            'X-RapidAPI-Host': 'us-zip-code-information.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        const js_obj = await response.json();
+        const string_json = JSON.stringify(js_obj);
+        const parse_json = JSON.parse(string_json);
+
+        const result = {
+            State: parse_json[0].State,
+            City_Town: parse_json[0].City
+        }
+
+        //console.log(fieldValues);
+
+        res.status(201).json({ fieldValues: result });
+
+    } catch (error) {
+        console.error(error);
+    }
+
+
+    //res.status(201).json({ message: 'success' });
+});
+
 
 
 app.listen(3000, () => {
